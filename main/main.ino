00f61ce2e948e045aaf8aa05ad8aea5db1af5d46
Filip Dragojlovic <filip.dragojlovic@tuwien.ac.at>
2026-01-24 18:48:34 +0100

    todo: calibrate turbidity, calibrate level, fix rgb values, fix vibration output

--- main/main.ino
+++ main/main.ino
@@ -4,23 +4,25 @@
 #define DELAY_MS 100 //TODO calibrate
 
 #define WH_PIN A0
-#define WH_THRESHOLD 8
+#define WH_THRESHOLD 30
 #define LS_LED_PIN A1
 #define LS_SENSOR_PIN A2
-#define LS_THRESHOLD 420 //TODO turbidity calibration
-#define RGB_R A3
-#define RGB_G A4
-#define RGB_B A5
+#define LS_THRESHOLD 170 //TODO turbidity calibration
+#define RGB_R 11
+#define RGB_G 10
+#define RGB_B 9
+#define VIB_PIN A5
+#define BUZZ_PIN 6
 
 //CONCURRENCY INIT
 // 0 water histogram
 // 1 light sensor
-int concurrency_input = 0;
+short int concurrency_input = 0;
 
 // 0 RGB LED
 // 1 buzzer
 // 2 vibration motor
-int concurrency_output = 0;
+short int concurrency_output = 0;
 
 //WATER HISTOGRAM INIT
 int wh_lastReading = 0;
@@ -36,18 +38,27 @@ bool ls_isMurky = false;
 
 //RGB LED OUTPUT
 
+//VIBRATION OUTPUT
+// 0 not vibrating
+// 1 low vibration
+// 2 high vibration
+short int vib_status = 0;
+
+//BUZZER OUTPUT
+bool is_buzzing = false;
+
 void turbidityReading() {
   int Lvalue = analogRead(LS_SENSOR_PIN); // read the light
   int mVolt = map(Lvalue, 0, 1023, 0, 5000); // map analogue reading to 5000mV
-  ls_isMurky = mVolt > LS_THRESHOLD;
+  ls_isMurky = mVolt < LS_THRESHOLD;
 
   Serial.print(mVolt);
-  Serial.print(" mV ");a
+  Serial.print(" mV ");
 }
 
 void waterLevelReading() {
   int reading = analogRead(WH_PIN);
-    int abs_change = abs(lastReading - reading);
+    int abs_change = abs(wh_lastReading - reading);
     wh_accumulated_change += abs_change - *wh_current;
     *wh_current = abs_change;
     wh_current = (wh_current == wh_end) ? wh_start : wh_current + 1;
@@ -67,13 +78,13 @@ void setColor(int r, int g, int b) {
 
 void updateRGB() {
   if ( !ls_isMurky && !wh_isMoving ) {
-    setColor(63, 63, 255);
+    setColor(0, 0, 255);
     Serial.print("CLEAN, STILL -> BLUE\n");
   } else if ( !ls_isMurky && wh_isMoving ) {
-    setColor(63, 255, 63);
+    setColor(0, 255, 0);
     Serial.print("CLEAN, MOVING -> GREEN\n");
   } else if ( ls_isMurky && !wh_isMoving ) {
-    setColor(105, 62, 7);
+    setColor(100, 60, 40);
     Serial.print("DIRTY, STILL -> BROWN\n");
   } else if (ls_isMurky && wh_isMoving) {
     setColor(255, 0, 0);
@@ -81,13 +92,39 @@ void updateRGB() {
   }
 }
 
+void updateVibration() {
+  short int prev = vib_status;
+
+  if(wh_isMoving) {
+    vib_status = ls_isMurky ? 2 : 1;
+  } else { vib_status = 0; }
+
+  if (vib_status != prev) {
+    switch (vib_status) {
+      case 0: analogWrite(VIB_PIN, LOW); break;
+      case 1: analogWrite(VIB_PIN, 100); break;
+      case 2: analogWrite(VIB_PIN, HIGH); break;
+    }
+  }
+}
+
+void updateBuzzer() {
+  if( ls_isMurky && !is_buzzing ) {
+    tone(BUZZ_PIN, 50);
+    is_buzzing = true;
+  } else if (!ls_isMurky && is_buzzing) {
+    noTone(BUZZ_PIN);
+    is_buzzing = false;
+    }
+}
+
 void setup() {
   //WATER HISTOGRAM
   pinMode(WH_PIN, INPUT); 
   Serial.begin(9600);
 
-  start = &wh_measurements_queue[0];
-  end = &wh_measurements_queue[9];
+  wh_start = &wh_measurements_queue[0];
+  wh_end = &wh_measurements_queue[9];
   wh_current = wh_start;
 
   //TURBIDITY SENSOR
@@ -103,7 +140,7 @@ void setup() {
 
 void loop() {
   if(concurrency_input == 0) {
-    Serial.print("WH ");
+    Serial.print("\nWH ");
 
     waterLevelReading();
     
@@ -111,9 +148,9 @@ void loop() {
     else { Serial.print(" Not moving"); }
 
   } else if (concurrency_input == 1) {
-    Serial.print("LS ");
+    Serial.print("\nLS ");
     
-    turbidity_reading();
+    turbidityReading();
 
     if(ls_isMurky) { Serial.print("Murky"); }
     else { Serial.print("Clear"); }
@@ -121,13 +158,18 @@ void loop() {
 
   concurrency_input = (concurrency_input + 1) % NO_INPUTS;
 
-  if(concurerncy_output == 0) {
-    Serial.print("RGB ");
+  if(concurrency_output == 0) {
+    Serial.print("\nRGB ");
     updateRGB();
-  } else if (concurrency_ouput == 1) {
-    Serial.print("BUZ ");
-  } else if (concurency_output == 2) {
-    Serial.print("VIB ");
+  } else if (concurrency_output == 1) {
+    Serial.print("\nBUZ ");
+    updateBuzzer();
+    if (is_buzzing) { Serial.print("Buzzing "); }
+    else { Serial.print('Not buzzing'); }
+    
+  } else if (concurrency_output == 2) {
+    Serial.print("\nVIB ");
+    updateVibration();
   }
 
   concurrency_output = (concurrency_output + 1) % NO_OUTPUTS;
